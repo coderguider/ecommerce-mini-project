56a514b47cc8f06d767e8e2f2e619d007aead4b2
Conor Guider <con.guider@gmail.com>
2019-03-29 15:37:29 +0000

    added wrong views.py - fixed

--- search/views.py
+++ search/views.py
@@ -1,35 +1,8 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
-def view_cart(request):
-    """A view that renders the cart contents page"""
-    return render(request, "cart.html")
-    
-    
-def add_to_cart(request, item_id):
-    """Add a quantity of the specified product to the cart"""
-    quantity=int(request.POST.get('quantity'))
-    
-    cart = request.session.get('cart', {})
-    if item_id in cart:
-        cart[item_id] = int(cart[item_id]) + quantity      
-    else:
-        cart[item_id] = cart.get(item_id, quantity) 
-    
-    request.session['cart'] = cart
-    return redirect(reverse('index'))
-    
-    
-def adjust_cart(request, item_id):
-    """Adjust the quantity of the specified product to the specified amount"""
-    quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
-    
-    if quantity > 0:
-        cart[item_id] = quantity
-    else:
-        cart.pop(item_id)
-        
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+def do_search(request):
+    products = Product.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "products.html", {"products":products})
