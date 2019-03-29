723eacd6c04d2bf19d649e52280be29ed06cbda8
Conor Guider <con.guider@gmail.com>
2019-03-29 16:24:40 +0000

    added css and fonts

--- cart/views.py
+++ cart/views.py
@@ -12,7 +12,10 @@ def add_to_cart(request, item_id):
     quantity=int(request.POST.get('quantity'))
     
     cart = request.session.get('cart', {})
-    cart[item_id] = cart.get(item_id, quantity) 
+    if item_id in cart:
+        cart[item_id] = int(cart[item_id]) + quantity      
+    else:
+        cart[item_id] = cart.get(item_id, quantity) 
     
     request.session['cart'] = cart
     return redirect(reverse('index'))
